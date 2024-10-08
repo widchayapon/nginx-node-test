97d3139cd501449b940b880fcca804db199b666b
Widchayapon Srisawad <widchayapon.sr@inet.co.th>
2024-10-08 10:33:38 +0700

    commit 5

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ app.use('/', express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-
+app.get('/test', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'test.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
